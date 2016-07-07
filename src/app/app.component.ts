0f24b0b7d840599f46ab62cc1aeb926a4281ddbd
Per Hornshøj-Schierbeck <per.hornshoejschierbeck@gmail.com>
2016-07-07 09:46:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  timeValue: number = 60;
+  timeCounter: number;
+  intervalRef: any;
+
+  constructor() {
+    this.timeCounter = this.timeValue;
+  }
+
+  startTimer() {
+    console.log('timeCounter clicked');
+    if (this.intervalRef) return;
+    this.intervalRef = setInterval(() => {
+      console.log('count down');
+      if (this.timeCounter > 0)
+        this.timeCounter--;
+    }, 1000);    
+  }
+
+  stopTimer() {
+    clearInterval(this.intervalRef);
+    this.intervalRef = null;
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.timeCounter = this.timeValue;
+  }
+
+  setTimeValue(value) {
+    this.timeValue += value;
+    this.resetTimer();
+  }
 }
